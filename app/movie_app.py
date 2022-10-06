ff7a5fe943a42051e0f847c76f56990e6dbc52c7
rashidatsalau <rashidat.salau@cloudreach.com>
2022-10-06 13:37:35 +0200

    Changes to movie_app

--- app/movie_app.py
+++ app/movie_app.py
@@ -2,9 +2,13 @@ import json
 
 from flask import Flask,render_template
 from flask_mysqldb import MySQL
+from flask import request
+
 
 app = Flask("MovieApp")
-app.config["MYSQL_HOST"] = "127.0.0.1"
+
+#app.config["MYSQL_HOST"] = "127.0.0.1"
+app.config["MYSQL_HOST"] = "DOCKER_CONTAINER_IP_PLACEHOLDER"
 app.config["MYSQL_USER"] = "root" 
 app.config["MYSQL_PASSWORD"] = "zatiwa1408" 
 app.config["MYSQL_DB"] = "movie_db" 
@@ -15,28 +19,67 @@ mysql = MySQL(app)
 
 
 @app.route("/")
+#Just to test that Flask is working with Hello World
 def hello_world():
     return "<p>Hello World!</p>"
 
 @app.route("/movies/")
 def list_movies():
     cursor = mysql.connection.cursor()
-    query_string = "SELECT * FROM movie_tbl"
+    query_string = "SELECT * FROM movie_tbl;"
     cursor.execute(query_string)
     data = cursor.fetchall()
     cursor.close()
-    return json.dumps(data)
+    return json.dumps(data) #Returns data as a string
 
 
 
 @app.route("/movies-table/")
 def list_movie_table():
     cursor = mysql.connection.cursor()
-    query_string = "SELECT * FROM movie_tbl"
+    query_string = "SELECT * FROM movie_tbl;"
     cursor.execute(query_string)
     data = cursor.fetchall()
     cursor.close()
-    return render_template("movies.html.tpl", movies_data = data)
+    return render_template("movies.html.tpl", movies_data = data) #Return data as rendered html template
+
+
+@app.route("/new-movie/")
+def new_movie(): 
+    cursor = mysql.connection.cursor()
+    d_query_string = "SELECT * FROM directors_tbl;"
+    a_query_string = "SELECT * FROM main_actors_tbl;"
+    cursor.execute(d_query_string)
+    directors_data = cursor.fetchall()
+    cursor.execute(a_query_string)
+    actors_data = cursor.fetchall()
+    cursor.close()
+    return render_template("add-movie.html.tpl",
+        directors_data=directors_data,
+        actors_data=actors_data
+    ) # Return data as rendered html template
+
+@app.route('/add-new-movie/', methods=['POST'])
+def add_new_movie():
+    cursor = mysql.connection.cursor()
+
+    # Collect info from Form
+    title = request.form['movietitle']
+    release_year = request.form['releaseyear']
+    director_id = request.form['directorSelect']
+    actor_id = request.form['directorSelect']
+
+    # INSERT new movie inside the table movie_tbl
+    movie_query_string = "INSERT INTO movie_tbl VALUES(null, %s, %s, %s);"
+    cursor.execute(movie_query_string, (title, release_year, director_id))
+    new_movie_id = cursor.lastrowid
+
+    # INSERT into the table movie_actors_tbl
+    movie_actor_query_string = "INSERT INTO movie_actors_tbl VALUES(%s, %s);"
+    cursor.execute(movie_actor_query_string, (new_movie_id, actor_id))
+    mysql.connection.commit()
+    cursor.close()
+    return list_movie_table()
 
 
 if __name__ == "__main__":
--- app/templates/movies.html.tpl
+++ app/templates/movies.html.tpl
@@ -1,13 +1,21 @@
-####<p>This is my rendered template</p>
-<table>
-<tr>
-    <th>Title</th>
-    <th>Year</th>
-</tr>
+{% include "header.html" %}
+<table class="table table-striped">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Release Year</th>
+        </tr>
+    </thead>
+<tbody>
 {% for movie in movies_data %}
-<tr>
+<tr>    
     <td>{{movie[1]}}</td>
     <td>{{movie[2]}}</td>
 </tr>
 {% endfor %}
+</tbody>
 </table>
+<div>
+    <a class="btn btn-primary" href="{{ url_for('new_movie') }}" role="button">Add new Movie</a>
+</div>
+{% include "footer.html" %}
